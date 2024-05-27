12dc95021f6bb947d7cc29e8f13a5e108efc19f6
rakeshkumar0016 <webexpert.pixxel@gmail.com>
2024-05-27 15:10:46 +0530

    ne code

--- src/components/animation/sections/Hero.jsx
+++ src/components/animation/sections/Hero.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import BannerVideo from "../../../assets/banner-video.mp4";
+import BannerVideo from "../../../assets/sample.mp4";
 import './Styles.css'
 
 const Hero = () => {
   return (
     <section className="relative h-full">
-      <video className="grayscale bannervideo" autoPlay muted loop>
+      <video className="bannervideo" autoPlay muted loop>
         <source src={BannerVideo} type="video/mp4" />
       </video>
     </section>
